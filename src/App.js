95c7713a358177cab5b2fee75c4cc3a2cdce7ada
Sameemul Haque K <samhaqk@gamil.com>
2023-05-07 00:39:50 +0530

    removed css ChatMessage updated

--- src/App.js
+++ src/App.js
@@ -72,9 +72,16 @@ function ChatPage(){
 }
 
 function ChatMessage(props){
-  const {text, uid} = props.message;
+  const {text, uid, photoURL} = props.message;
 
-  return <p>{text}</p>
+  const messageClass = uid === auth.currentUser.uid ? 'sent': 'received';
+
+  return(
+    <div className={'message ${messageClass}'}>
+      <img src={photoURL} />
+      <p>{text}</p>
+    </div>
+  )
 }
 
 export default App;
