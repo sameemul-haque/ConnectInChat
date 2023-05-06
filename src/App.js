ac3a900deab897d821a8941e1da56172d2c363cc
Sameemul Haque K <samhaqk@gamil.com>
2023-05-07 00:20:03 +0530

    ChatPage

--- src/App.js
+++ src/App.js
@@ -52,7 +52,23 @@ function SignOut(){
 }
 
 function ChatPage(){
+  const messagesRef = firestore.collection('messages');
+  const query = messagesRef.orderBy('createdAt').limit(25);
+  
+  const [messages] = useCollectionData(query, {idField: 'id'});
 
+  return(
+    <>
+      <div>
+        {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+      </div>
+
+      <div>
+        
+      </div>
+    </>
+  )
 }
+
 
 export default App;
