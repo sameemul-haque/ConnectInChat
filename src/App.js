1ca67b9ae113c88d1eba94f4dba73e646618ea44
Sameemul Haque K <samhaqk@gamil.com>
2023-05-06 23:55:18 +0530

    firebase.initializeApp({//config//})

--- src/App.js
+++ src/App.js
@@ -6,6 +6,19 @@ import 'firebase/auth';
 import {useAuthState} from 'react-firebase-hooks/auth';
 import {useCollectionData} from 'react-firebase-hooks/firestore';
 
+firebase.initializeApp({
+  apiKey: "AIzaSyC0EeYEz6RcXogSKMzGXNqLbN_JUWp1iYA",
+  authDomain: "connectinchat.firebaseapp.com",
+  projectId: "connectinchat",
+  storageBucket: "connectinchat.appspot.com",
+  messagingSenderId: "1001698473625",
+  appId: "1:1001698473625:web:f04ffb3ba64cd8720b6a08",
+  measurementId: "G-86DBDW9R4M"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
 function App() {
   return (
     <div className="App">
