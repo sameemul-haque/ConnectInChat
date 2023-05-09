ff10b557dc07b8c6ccfa2e0d67e694eca25b3128
sameemul-haque <110324374+sameemul-haque@users.noreply.github.com>
2023-05-09 23:08:55 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -11,13 +11,7 @@ import {useCollectionData} from 'react-firebase-hooks/firestore';
 import { FcGoogle } from "react-icons/fc";
 
 firebase.initializeApp({
-  apiKey: "AIzaSyC0EeYEz6RcXogSKMzGXNqLbN_JUWp1iYA",
-  authDomain: "connectinchat.firebaseapp.com",
-  projectId: "connectinchat",
-  storageBucket: "connectinchat.appspot.com",
-  messagingSenderId: "1001698473625",
-  appId: "1:1001698473625:web:f04ffb3ba64cd8720b6a08",
-  measurementId: "G-86DBDW9R4M"
+  //
 })
 
 const auth = firebase.auth();
@@ -67,7 +61,7 @@ function ChatPage(){
   const dummy = useRef() 
 
   const messagesRef = firestore.collection('messages');
-  const query = messagesRef.orderBy('createdAt').limit(25);
+  const query = messagesRef.orderBy('createdAt').limitToLast(25);
   
   const [messages] = useCollectionData(query, {idField: 'id'});
 
