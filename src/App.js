c041239745bc120028df609857899c70a926c2cc
sameemul-haque <110324374+sameemul-haque@users.noreply.github.com>
2023-06-08 17:32:44 +0530

    created a UserMenu component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
-import SignIn from "./components/SignIn";
-import SignOut from "./components/SignOut";
-import UsernameChecker from "./components/UsernameChecker";
-import ChatPage from "./components/ChatPage";
 import { ReactComponent as Logo } from "./logo.svg";
 import { auth } from "./firebase-config.js";
 import { useAuthState } from "react-firebase-hooks/auth";
+import SignIn from "./components/SignIn";
+import UsernameChecker from "./components/UsernameChecker";
+import ChatPage from "./components/ChatPage";
+import UserMenu from "./components/UserMenu";
 
 function App() {
   const [user] = useAuthState(auth);
@@ -14,7 +14,7 @@ function App() {
     <div className="App">
       <header>
         <Logo className="img" />
-        <SignOut />
+        <UserMenu user={user} />
       </header>
       <section>
         {user ? (
---
+++ src/components/UserMenu.js
@@ -0,0 +1,8 @@
+import { Icon } from "@iconify/react";
+import SignOut from "./SignOut";
+
+function UserMenu() {
+  return <>{<SignOut />}</>;
+}
+
+export default UserMenu;
