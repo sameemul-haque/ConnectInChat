9c39bb13929f698af698bc179d5e2667f55207e5
sameemul-haque <110324374+sameemul-haque@users.noreply.github.com>
2023-06-09 23:35:43 +0530

    added a side bar

--- src/components/UserMenu.js
+++ src/components/UserMenu.js
@@ -1,8 +1,23 @@
 import { Icon } from "@iconify/react";
 import SignOut from "./SignOut";
+import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
 
 function UserMenu() {
-  return <>{<SignOut />}</>;
+  return (
+    <>
+      <Sidebar>
+        <Menu>
+          <SubMenu label="Charts">
+            <MenuItem> {<SignOut />} </MenuItem>
+            <MenuItem> Line charts </MenuItem>
+          </SubMenu>
+          <MenuItem> Documentation </MenuItem>
+          <MenuItem> Calendar </MenuItem>
+        </Menu>
+      </Sidebar>
+      ;
+    </>
+  );
 }
 
 export default UserMenu;
